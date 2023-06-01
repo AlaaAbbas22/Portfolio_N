6c19d1adb7d83f4bf27e641d2c4a9eb072f7c58c
AlaaAbbas22 <alaa@uni.minerva.edu>
2023-06-01 18:09:52 +0300

    Third day

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
 import './App.css'
 import bg from './assets/bg.mp4'
 import Nav from './components/nav'
+import "animate.css/animate.min.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <video autoPlay muted loop id="bg" className='filter'>
+          <source src={bg} type="video/mp4"/>
+        </video>
+        <Nav/>
+        <Outlet/>
+      </>
+    ),
+    children:
+    [{
+      path: "/",
+      element: <main className="relative -top-20">
+      <div className="min-h-screen flex">
+        <h1 className='writer writer-text text-white self-center mx-auto'>Welcome to <span className='text-blue-400'>Alaa</span>
+        </h1>
+      </div>
+      <div className="min-h-screen ">
+        <div className='ring-2 rounded-xl bg-slate-300'>
+        Hello, there! 
+        <p>
+        I am Alaa, but you can call me LOL.
+        </p> 
+        </div>
+      </div>
+    </main>
+    }]
+  },
+]);
+
+
 
 function App() {
   return (
     <>
-      
-      <video autoPlay muted loop id="bg" className='filter'>
-        <source src={bg} type="video/mp4"/>
-      </video>
-      <Nav/>
-      <main className="relative -top-20">
-        <div className="min-h-screen flex">
-          <h1 className='writer writer-text text-white self-center mx-auto'>Welcome to <span className='text-blue-400'>Alaa</span>
-          </h1>
-        </div>
-      </main>
+     <RouterProvider router={router} />  
     </>
   )
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,33 +1,36 @@
 import {useState} from "react"
 import icon from "../assets/favicon.ico"
+import {Link} from "react-router-dom"
 
 const Nav = () => {
     const [mob, setmob] = useState(false)
+    const [isMounted, setIsMounted] = useState(false);
 
-    const mountedStyle = { animation: "inAnimation 450ms ease-in" };
+    const mountedStyle = { animation: "inAnimation 450ms ease-in", background: "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))"};
   const unmountedStyle = {
     animation: "outAnimation 470ms ease-out",
-    animationFillMode: "forwards"
+    animationFillMode: "forwards",
+    background: "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))",
   };
     return (
     <> <header className="sticky top-8 z-50">
         <nav className="max-w-full" >
-            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto '>
-            <div className="w-[50%]"> <a href="/"> <img className='rounded-full relative w-[10%]' src={icon}></img></a></div>
+            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto'>
+            <div className="w-[50%]"> <Link to="/"> <img className='rounded-full relative w-[10%] ring-1 ring-slate-900 shadow-2xl' src={icon}></img></Link></div>
             
-            <button type="button" onClick={()=>{setmob(!mob)}} className="p-2  text-gray-500 rounded-lg focus:outline-none ring-2 ring-gray-400 " >
+            <button type="button" onClick={() => {setIsMounted(!isMounted); if (!mob) setmob(true);}} className="p-2 shadow-2xl text-gray-500 rounded-lg focus:outline-none ring-2 ring-gray-400 " >
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button></div>
-            {mob&&<div id="Bar" className="items-center justify-between" style={mob ? mountedStyle : unmountedStyle}>
-      <ul id= "Bar" onFocus={()=>{setmob(true)}} className="flex flex-col font-medium border border-gray-100 rounded-lg  fixed right-10" style={{backgroundColor: "rgba(0,0,0,.3)"}}>
+            {mob&&<div id="Bar" className="items-center justify-between" >
+      <ul id= "Bar" style={isMounted ? mountedStyle : unmountedStyle} onAnimationEnd={() => { if (!isMounted) setmob(false) }} className="flex flex-col font-medium border border-gray-100 rounded-lg  fixed right-10" >
         <li onClick={()=>{setmob(!mob)}}>
-          <a href="/" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Home</a>
+          <Link to="/" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Home</Link>
         </li>
         <li onClick={()=>{setmob(!mob)}}>
-          <a  href="/work" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Work</a>
+          <Link to="/work" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Work</Link>
         </li>
         <li onClick={()=>{setmob(!mob)}}>
-          <a href="/contact" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Contact</a>
+          <Link to="/contact" className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 ">Contact</Link>
         </li>
       </ul>
     </div>}
