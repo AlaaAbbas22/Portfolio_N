5a5168a33e8fd0bf1384a704554d9ec4f7dc263f
AlaaAbbas22 <alaa@uni.minerva.edu>
2023-06-04 10:33:51 +0300

    P

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,14 @@ const router = createBrowserRouter([
         <h1 className='writer writer-text text-white self-center mx-auto'>Welcome to <span className='text-blue-400'>Alaa</span>
         </h1>
       </div>
-      <div className="min-h-screen ">
-        <div className='ring-2 rounded-xl bg-slate-300 font-mono tracking-widest text-xl leading-loose'>
+      <div className="min-h-screen">
+        <div className=' font-mono  text-xl mx-auto pt-40'>
          
-        <p className='mx-80 text-justify py-4 mt-40'>
+        <p className='mx-auto text-justify py-8 ring-2 rounded-xl bg-slate-100 leading-loose tracking-widest px-10 md:w-2/3'>
         <center>Hello, there! </center>
-        I am Alaa, but you can call me LOL. I am a Computer Science major at Minerva University, headquartered in San Francisco, CA. I don't know if
-        I am considered a software engineer, but I hope so. My best friend is Stackoverflow.
+        I am <span className='text-red-600'>Alaa</span>, but you can call me <span className='text-red-600'>LOL</span>. I am a Computer Science major at Minerva University, headquartered in San Francisco, CA. I don't know if
+        I am considered a software engineer, but I hope so. My best friend is Stackoverflow. I like programming jokes. Some people think that all
+        programming jokes are bad jokes. Not all of them, just <span className='text-yellow-500'>sum(<span className='text-blue-900'>them</span>)</span>.
         </p> 
         </div>
       </div>
