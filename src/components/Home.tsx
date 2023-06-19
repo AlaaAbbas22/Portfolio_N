6d51178c752857e26372c27b731669d57ea228ef
AlaaAbbas22 <alaa@uni.minerva.edu>
2023-06-18 20:26:59 -0500

    Last one

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,15 +7,15 @@ const Home = () => {
   return (
     <main className="relative -top-20">
       <div className="min-h-screen  pt-40">
-        <img src={home} className='mx-auto'></img>
-        <h1 className='writer writer-text text-white self-center mx-auto'>Welcome to <span className='text-blue-400'>Alaa</span>
+        <img src={home} className='mx-auto sm:w-auto w-1/2'></img>
+        <h1 className='writer writer-text text-white self-center mx-auto text-2xl sm:text-5xl'>Welcome to <span className='text-blue-400'>Alaa</span>
         </h1>
         <div><button className='mx-auto animate-bounce mt-20 px-4 py-3 shadow-2xl rounded-full' onClick={()=>{first.current?.scrollIntoView({ behavior: "smooth"})}}>&#8595;</button></div>
       </div>
       <div ref={first} className="min-h-screen">
-        <div className=' font-mono  text-xl mx-auto pt-40'>
+        <div className=' font-mono  sm:text-xl text-sm mx-auto pt-40'>
          
-        <p className='mx-auto text-justify py-8 ring-2 rounded-xl bg-slate-100 leading-loose tracking-widest px-10 md:w-2/3'>
+        <p className='mx-auto text-justify sm:py-8 py-2 ring-2 rounded-xl bg-slate-100 leading-loose tracking-widest sm:px-10 px-3 md:w-2/3'>
         <center>Hello, there! </center>
         I am <span className='text-red-600'>Alaa</span>, but you can call me <span className='text-red-600'>LOL</span>. I am a Computer Science major at Minerva University, headquartered in San Francisco, CA. I don't know if
         I am considered a software engineer, but I hope so. My best friend is Stackoverflow. I like programming jokes. Some people think that all
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -16,7 +16,7 @@ const Nav = () => {
     <> <header className="sticky top-8 z-50">
         <nav className="max-w-full" >
             <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto'>
-            <div className="w-[50%]"> <Link to="/"> <img className='rounded-full relative w-[10%] ring-1 ring-slate-900 shadow-2xl' src={icon}></img></Link></div>
+            <div className="w-[50%]">  <img className='rounded-full relative w-[10%] ring-1 ring-slate-900 shadow-2xl invisible  ' src={icon}></img></div>
             
             <button type="button" onClick={() => {setIsMounted(!isMounted); if (!mob) setmob(true);}} className="p-2 shadow-2xl text-gray-500 rounded-lg focus:outline-none ring-2 ring-gray-400 " >
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
